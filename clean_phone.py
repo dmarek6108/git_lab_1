c33e97c012125a1e1f77d8919d65a886f69972b8
Donald Marek <dmarek@ND.EDU>
2020-11-16 19:52:28 -0500

    Adding regular experssion for phone valid

--- clean_phone.py
+++ clean_phone.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-def validate_phone(phone_number):
-    return True
-print(validate_phone(True))
 
+def validate_phone(phone_number):
+    """ Ensure that phone numbers are in a valid format
+
+    Keyword arguments:
+    phone_number -- A Pandas Series of phone_numbers as strings.
+
+    Returns: A boolean Pandas Series. Valid phone numbers are True, Invalid are False
+    """
+    bool_phone = phone_number.str.contains(r'^\d{3}[-]?\d{2}[-]?\d{3}')
+    return bool_phone
+numbers = pd.Series(['303-317-7129', '303-913-0349', '423-23-323'])
